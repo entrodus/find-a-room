717a303ab7e01a68f214445f499712ae78f86680
Nikos - symphonic <nikos.tsokos@symphonicosoft.com>
2018-08-24 17:58:59 +0100

    introduce dynamic id for input, targetable through label for

--- src/app/components/selectable-calendar.component.ts
+++ src/app/components/selectable-calendar.component.ts
@@ -11,9 +11,19 @@ export class SelectableCalendarComponent implements OnInit {
   @Input() selected: boolean;
   @Output() selectedChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  public id = `sc-${this.uuidv4()}`;
+
   constructor() { }
 
   public ngOnInit(): void {
+  }
+
+  public uuidv4(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      // tslint:disable-next-line:no-bitwise
+      const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
 
 }
